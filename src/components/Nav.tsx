95351851ed7ff08911a5c7f2c2418d42c7c2834b
HarshKotwal <harshkotwal1608@gmail.com>
2026-01-01 17:11:55 +0530

    navbar changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
   return (
-    <header className="flex justify-between items-center m-4 p-4 border border-gray-100 rounded-4xl bg-white px-5">
+    <header className="flex justify-between items-center mt-5 mx-40 p-2 border border-gray-100 shadow-xl rounded-4xl bg-white px-5">
       <div className="flex items-center-safe gap-3 ml-4">
         <img
           className="h-10 w-10 object-cover"
-          src="/logo.png"
+          src="/logo1.png"
           alt="ResuScan logo "
         />
         <h1
-          className="text-3xl bg-linear-to-r from-blue-300 to-blue-950 bg-clip-text text-transparent"
-          style={{ fontFamily: "'Archivo Black', sans-serif" }}
+          className="text-2xl bg-linear-to-r from-lime-400 to-cyan-600 bg-clip-text text-transparent"
+          style={{ fontFamily: "'Archivo Black', sans-serif " }}
         >
           ResuScanner
         </h1>
@@ -18,10 +18,10 @@ export default function Navbar() {
         className="flex items-center gap-5 mr-4"
         style={{ fontFamily: "'Inter',sans-serif" }}
       >
-        <button className="px-4 py-2 text-blue-900 font-extrabold hover:text-blue-300 hover:scale-110 transition-colors">
+        <button className="px-4 py-2 text-cyan-600 font-extrabold hover:text-lime-400 hover:scale-110 transition-colors">
           Register
         </button>
-        <button className="px-6 py-2 bg-linear-to-r from-blue-300 to-blue-900 text-white  rounded-lg hover:scale-105 transition-all font-bold">
+        <button className="px-4 py-2 bg-linear-to-r from-lime-400 to-cyan-600 text-white  rounded-lg hover:scale-105 transition-all font-bold">
           Sign in
         </button>
       </div>
