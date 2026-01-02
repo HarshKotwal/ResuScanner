c0954098883eabce6e240f10458d4f1206d53331
HarshKotwal <harshkotwal1608@gmail.com>
2026-01-02 20:47:23 +0530

    hero almost done

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ export default function Main() {
           ~ ATS Resume/CV scanner system
         </span>
         <div className="pt-10 pr-11 text-5xl leading-15 font-extrabold ">
-          Will your Resume survive those industry level ATS Bots?
+          Can your Resume survive those industry level ATS Bots?
         </div>
         <div className="pt-7 font-semibold ">
           75% of candidates worldwide never know why they're been rejected. The
@@ -15,18 +15,22 @@ export default function Main() {
           get into your dream job in 60 seconds.
         </div>
       </div>
-      <div className="w-200 pt-20 px-20 ">
-        <div className="p-8 border border-lime-300  shadow-lg shadow-lime-300/50 rounded-4xl">
+      <div className="w-200 pt-20 px-20 flex justify-center">
+        <div className="p-8 border border-gray-300  shadow-lg shadow-gray-700/50 rounded-4xl text-center">
           <div className="flex justify-center text-center text-2xl font-extrabold">
             Know your actual ATS score before recruiters do.
           </div>
-          <div className="pt-3 font-light text-center">
+          <div className="mt-5 font-light text-center">
             Our scanner reveals hidden formatting issues, missing keywords, and
             structural gaps that block ATS shortlisting.
           </div>
-          <button className="pt-15 ">Upload Your Resume</button>
-          <div>privacy garunteed</div>
-          <div>Supported formats: PDF. | Max file size: 5MB.</div>
+          <button className="mt-15 border-2 p-4 rounded-4xl bg-cyan-950 text-white font-extrabold text-center">
+            Upload Your Resume
+          </button>
+          <div className="font-light text-xs mt-4">privacy gauranteed</div>
+          <div className="text-sm font-bold mt-1">
+            Supported formats: PDF | Max file size: 5MB
+          </div>
         </div>
       </div>
     </div>
