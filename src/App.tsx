cd6e33f71be638a027edbfddbd34df8540d546b8
HarshKotwal <harshkotwal1608@gmail.com>
2026-01-07 21:57:57 +0530

    generated the resume card section with almost everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ResumeCard from "./components/ResumeCard";
+import { resumes } from "./constants";
 
 function App() {
   return (
@@ -8,13 +10,23 @@ function App() {
 
       <section className="main-section">
         <div className="page-handling">
-          <h1>Know Your ATS Score Before Recruiters Do</h1>
-          <h2>
+          <h1 className="text-center">
+            Know Your ATS Score Before Recruiters Do
+          </h1>
+          <h2 className="text-center">
             Analyze your submission with AI powered feedback that breaks down
             performance, issues, and optimization opportunities.
           </h2>
         </div>
       </section>
+
+      {resumes.length > 0 && (
+        <div className="resume-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
